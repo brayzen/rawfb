77b5b9ed3dae62a11a07e5f2f7fe10c7c4d939bb
brayzen <brian.ray.biz@gmail.com>
2017-09-01 20:29:03 -0700

    add stars, add being tags

--- assets/js/fbForm.js
+++ assets/js/fbForm.js
@@ -1,10 +1,18 @@
+// Feedback form
+//
+//
 $(function(){
+
+	var selectedWaysOfBeing = [];
+
+	// Button to toggle anonymity
 	$('#testimony-button, #anonymous-button').on('click', function(){
 		console.log("heya");
 		$('.testimony-display-1').toggleClass("hide");
 		$('.testimony-display-2').toggleClass("hide");
 	})
 
+	// Read Reciept hide/show for signin button
 	$('input:checkbox, #read-receipt').change(function(){
 		if ($(this).is(':checked'))	{
 			$('#button-signin').show()	
@@ -12,4 +20,17 @@ $(function(){
 			$('#button-signin').hide()	
 		}
 	})
+
+	//Being selection added to box
+	$('select#being-select').change(function(){
+		var val = $(this).val();	
+		if (selectedWaysOfBeing.indexOf(val) > -1)
+			return;
+		else
+			selectedWaysOfBeing.push(val);
+		console.log(val);
+		$('.being-box').append('<div class="being-square">'+ val + '</div>');	
+	})
+
+
 })
