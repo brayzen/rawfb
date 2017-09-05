6f7cef278ed8da21e38b337c0b530e19d4e300eb
brayzen <brian.ray.biz@gmail.com>
2017-09-05 00:12:24 -0700

    added feedbackCompiler WIP, stars display 5 stars default

--- assets/js/fbForm.js
+++ assets/js/fbForm.js
@@ -3,16 +3,35 @@
 //
 $(function(){
 
-	var selectedWaysOfBeing = [],
-			likableRating,
-			trustworthyRating,
-			toFirstName,
-			toLastName,
-			toEmail,
-			message,
-			fromFirstName,
-			fromLastName;
+	var FeedbackCompiler = function(){
+	  this.selectedWaysOfBeing = [] 
+		this.likableRating = 5 
+		this.trustworthyRating = 5
+		this.toFirstName = ''
+		this.toLastName = ''
+		this.toEmail = ''
+		this.message = ''
+		this.fromFirstName = ''
+		this.fromLastName = '' 
+		this.fromUserId = null
+	}
 
+	FeedbackCompiler.prototype.setData = function(){
+		this.toFirstName = $('#first-name').val()	
+		this.toLastName = $('#last-name').val()	
+		this.toEmail = $('#email').val()	
+		this.likableRating = $('#rating-likable').val()	
+		this.trustworthyRating = $('#rating-trustworthy').val()	
+		this.message = $('#message').val()	
+	}
+	
+	FeedbackCompiler.prototype.postDataToServer = function(){
+		io.socket.post('/feedback', this, function(resData, jwRes){
+			console.log(jwRes)	
+		})	
+	}
+
+ var fb = new FeedbackCompiler()	
 	// Button to toggle anonymity
 	$('#testimony-button, #anonymous-button').on('click', function(){
 		console.log("heya");
@@ -32,13 +51,20 @@ $(function(){
 	//Being selection added to box
 	$('select#being-select').change(function(){
 		var val = $(this).val();	
-		if (selectedWaysOfBeing.indexOf(val) > -1)
+		if (fb.selectedWaysOfBeing.indexOf(val) > -1)
 			return;
 		else
-			selectedWaysOfBeing.push(val);
+			fb.selectedWaysOfBeing.push(val);
 		console.log(val);
 		$('.being-box').append('<div class="being-square">'+ val + '</div>');	
 	})
 
+	//Submit button send off
+	$('#button-submit').click(function(){
+		fb.setData();
+		console.log(fb);	
+		fb.postDataToServer();
+
+	})
 
 })
